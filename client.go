94428094821c5e7681f2bea5854639093b167c73
RImangali <158676473+RImangali@users.noreply.github.com>
2024-05-02 23:43:25 +0500

    initial commit

--- client.go
+++ client.go
@@ -2,50 +2,42 @@ package main
 
 import (
     "context"
-    "log"
+	"fmt"
+	"log"
+    
+	"google.golang.org/grpc"
 
-    "google.golang.org/grpc"
-
-    pb "go_assignment_4/user"
+	pb "go_assignment_4/user"
 )
 
 func main() {
-    conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
-    if err != nil {
-        log.Fatalf("did not connect: %v", err)
-    }
-    defer conn.Close()
-    
-    c := pb.NewUserServiceClient(conn)
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to dial server: %v", err)
+	}
+	defer conn.Close()
 
-    // AddUser
-    addUserResponse, err := c.AddUser(context.Background(), &pb.User{
-        Id:    1,
+	client := pb.NewUserServiceClient(conn)
+
+	addUserResponse, err := client.AddUser(context.Background(), &pb.User{
+		Id:    1,
         Name:  "Rakhatuly Imangali",
         Email: "210107008@stu.sdu.edu.kz",
-    })
-    if err != nil {
-        log.Fatalf("AddUser failed: %v", err)
-    }
-    log.Printf("User added with ID: %d", addUserResponse.Id)
+	})
+	if err != nil {
+		log.Fatalf("AddUser failed: %v", err)
+	}
+	fmt.Printf("AddUser response: %v\n", addUserResponse)
 
-    // GetUser
-    getUserResponse, err := c.GetUser(context.Background(), &pb.UserID{Id: 1})
-    if err != nil {
-        log.Fatalf("GetUser failed: %v", err)
-    }
-    log.Printf("User retrieved: %+v", getUserResponse)
+	getUserResponse, err := client.GetUser(context.Background(), &pb.UserID{Id: 1})
+	if err != nil {
+		log.Fatalf("GetUser failed: %v", err)
+	}
+	fmt.Printf("GetUser response: %v\n", getUserResponse)
 
-    // ListUsers
-    stream, err := c.ListUsers(context.Background(), &pb.Empty{})
-    if err != nil {
-        log.Fatalf("ListUsers failed: %v", err)
-    }
-    for {
-        user, err := stream.Recv()
-        if err != nil {
-            log.Fatalf("Error receiving user: %v", err)
-        }
-        log.Printf("User: %+v", user)
-    }
+	listUsersResponse, err := client.ListUsers(context.Background(), &pb.Empty{})
+	if err != nil {
+		log.Fatalf("ListUsers failed: %v", err)
+	}
+	fmt.Printf("ListUsers response: %v\n", listUsersResponse)
 }
